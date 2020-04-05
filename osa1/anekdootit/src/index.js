2fb5d58f60523385068776d7f0a0ca453f53a935
Timo Viipuri <timo.t@viipuri.eu>
2020-04-05 23:57:45 +0300

    Osa 1: Anekdootit step 2 done

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -2,25 +2,41 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
 
-  const clickHandler = () => {
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0))
+
+  const nextAnecdotePressed = () => {
     const anecdoteIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(anecdoteIndex)
   }
+
+  const votePressed = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    console.log("points: ", copy)
+    setPoints(copy)
+  }
+
   return (
     <div>
-      {props.anecdotes[selected]}
-      <Button text="next anecdote" handler={clickHandler} />
+      <div>
+        {props.anecdotes[selected]}
+      </div>
+      <div>
+        has {points[selected]} points
+      </div>
+      <Button text="vote" handler={votePressed} />
+      <Button text="next anecdote" handler={nextAnecdotePressed} />
     </div>
   )
 }
 
 const Button = (props) => {
   return (
-    <div>
+    <>
       <button onClick={props.handler}> {props.text} </button>
-    </div>
+    </>
   )
 }
 
