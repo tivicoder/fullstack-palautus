d7026baa9a9632debf5190ec32c2d3f86b26b6f6
Timo Viipuri <timo.t@viipuri.eu>
2020-04-08 09:41:44 +0300

    Osa 2.12* maiden tiedot, step1 done

---
+++ osa2/maiden_tiedot/src/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+const App = () => {
+  const [filter, setFilter] = useState("")
+  const [countries, setCountries] = useState([])
+
+  const filterChanged = (event) => setFilter(event.target.value)
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      find countries <input value={filter} onChange={filterChanged}/>
+      <CountryList filter={filter} countries={countries} />
+    </div>
+  )
+}
+
+const CountryList = ({filter, countries}) => {
+  const filteredCountries = () => {
+    const filteredCountries = countries.filter(country =>
+      country.name.toLowerCase().startsWith(filter.toLowerCase()))
+    if (filteredCountries.length > 10) {
+      return "Too many matches, specify another filter"
+    } else if (filteredCountries.length === 1) {
+      return <Country country={filteredCountries[0]} />
+    } else {
+      return filteredCountries.map((country) => <div key={country.name}> {country.name} </div>)
+    }
+  }
+
+  return (
+    <div>
+      {filteredCountries()}
+    </div>
+  )
+}
+
+const Country = ({country}) => {
+  const languageList = () => {
+    return (
+      <ul>
+        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+      </ul>
+    )
+  }
+
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h2> languages</h2>
+      {languageList()}
+      <img src={country.flag} alt="flag" width="100"/>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
