81833a0497ceb607f1d92c18423ea6e2df533aaf
Timo Viipuri <timo.t@viipuri.eu>
2020-04-14 14:38:24 +0300

    Part 4.2 blogilista, step2: separated app.js. Task 4.2 done

--- osa4/index.js
+++ osa4/index.js
@@ -1,30 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
 const log = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
 
+const server = http.createServer(app)
 
-log.info('url: ', config.MONGODB_URI)
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    log.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    log.error('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use('/api/blogs', blogsRouter)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   log.info(`Server running on port ${config.PORT}`)
 })
