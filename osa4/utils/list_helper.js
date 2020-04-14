035f3f8a78d14e33c426ba611ca7e08e8847a68a
Timo Viipuri <timo.t@viipuri.eu>
2020-04-14 20:57:19 +0300

    Part 4.4: apufunktioita ja yksikkötestejä, step2 done

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,12 @@
-const dummy = (blogs) => {
+const dummy = (dummy) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, item) => { return sum + item.likes }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
