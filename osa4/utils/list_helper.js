4e223a0564837bf16e31d6dafcae08e01c382118
Timo Viipuri <timo.t@viipuri.eu>
2020-04-14 23:24:10 +0300

    Part 4.5*: apufunktioita ja yksikkötestejä, step3 done
    Also added launch.json for VSCode debugger

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, item) => { return sum + item.likes }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  let highest = blogs[0]
+  for (let blog of blogs) {
+    if (blog.likes > highest.likes) highest = blog
+  }
+
+  return { title: highest.title, author: highest.author, likes: highest.likes }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
