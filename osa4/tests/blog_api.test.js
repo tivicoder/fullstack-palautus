24fe5d741943837922e308cac2973faa55f640ed
Timo Viipuri <timo.t@viipuri.eu>
2020-04-15 11:35:16 +0300

    4.8: blogilistan testit, step 1: added tests for GET

---
+++ osa4/tests/blog_api.test.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+describe('api get', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('correct amount of blogs is returned', async () => {
+    const response =  await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.listWithManyBlogs.length)
+  })
+
+  test('a specific blog is among the returned blogs', async () => {
+    const response =  await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('TDD harms architecture')
+  })
+})
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.listWithManyBlogs)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- osa4/utils/config.js
+++ osa4/utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI = process.env.MONGODB_URI
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
 const PORT = process.env.PORT
 
 module.exports = { MONGODB_URI, PORT }
