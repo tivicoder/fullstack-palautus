399b92fb31e7814571fe338ac0d06ed626525c1b
Timo Viipuri <timo.t@viipuri.eu>
2020-04-21 13:35:40 +0300

    5.7* blogilistan frontend, step7 done

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [expanded, setExpanded] = useState(false)
+
+  const viewClicked = (event) => {
+    setExpanded(!expanded)
+  }
+
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <label type='text' id='textbox_id' onClick={viewClicked}>{blog.title} </label>
+      {blog.author}
+      <button type='button' onClick={viewClicked}>{ expanded ? 'hide' : 'view' }</button>
+      <div style={showWhenExpanded}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button type='button'>like</button> </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
