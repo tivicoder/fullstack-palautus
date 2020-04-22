ec7725701c4151028cbb37516fe9ae6a1d243186
Timo Viipuri <timo.t@viipuri.eu>
2020-04-22 09:44:34 +0300

    5.9*: blogilistan frontend, step9 done

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -106,9 +106,11 @@ const App = () => {
       <Togglable buttonLabel='new blog' ref={blogFormRef} >
         <NewBlogForm addBlog={addBlog} likeBlog={likeBlog} />
       </Togglable>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} likeBlog={id => likeBlog(blog.id)} />
-      )}
+      {[...blogs]
+        .sort((a,b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} likeBlog={id => likeBlog(blog.id)} />
+      ) }
     </div>
   )
 }
