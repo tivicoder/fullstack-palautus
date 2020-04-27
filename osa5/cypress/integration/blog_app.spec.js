5d4d80fdf7185157c0c89ee05ffebabead886870
Timo Viipuri <timo.t@viipuri.eu>
2020-04-27 15:17:55 +0300

    5.19: blogilistan end to end -testit, step3 done

--- osa5/cypress/integration/blog_app.spec.js
+++ osa5/cypress/integration/blog_app.spec.js
@@ -1,7 +1,7 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
-    cy.createUser({ name:'test name', username:'test username', password:'test password'})
+    cy.createUser({ name:'test name', username:'test username', password:'test password' })
     cy.visit('http://localhost:3000')
   })
 
@@ -26,6 +26,23 @@ describe('Blog app', function() {
       cy.get('button').contains('login').click()
       cy.contains('wrong username or password')
         .should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'test username', password: 'test password' })
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('new blog').click()
+      cy.get('#title').type('new title')
+      cy.get('#author').type('new author')
+      cy.get('#url').type('new url')
+      cy.contains('create').click()
+      cy.contains('a new blog new title by new author added')
+        .should('have.css', 'color', 'rgb(0, 128, 0)')
+      cy.contains('new title new author')
     })
   })
 })
--- osa5/cypress/support/commands.js
+++ osa5/cypress/support/commands.js
@@ -29,3 +29,12 @@ Cypress.Commands.add('createUser', ({ name, username, password }) => {
     name, username, password
   })
 })
+
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3001/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
+    cy.visit('http://localhost:3000')
+  })
+})
