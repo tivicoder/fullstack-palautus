0021edd5dbc6cc065630705c4eff25f7710af8c6
Timo Viipuri <timo.t@viipuri.eu>
2020-05-15 14:10:33 +0300

    6.13 anekdootit ja backend, step1 done

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import AnecdoteForm from './components/anecdoteForm'
 import AnecdoteList from './components/anecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
