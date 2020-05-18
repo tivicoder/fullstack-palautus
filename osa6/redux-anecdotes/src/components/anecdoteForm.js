228f9b3371f629c91071fd79849a40da69ff47a8
Timo Viipuri <timo.t@viipuri.eu>
2020-05-18 20:15:28 +0300

    6.20 anekdootit ja connect, step2 done

--- osa6/redux-anecdotes/src/components/anecdoteForm.js
+++ osa6/redux-anecdotes/src/components/anecdoteForm.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
-const AnecdoteForm = () => {
-  const dispatch = useDispatch()
-
+const AnecdoteForm = (props) => {
   const create = async (event) => {
     event.preventDefault()
     console.log('createNew: ', event.target.anecdote.value)
     const anecdote = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(anecdote))
-    dispatch(setNotification(`you created '${anecdote}'`, 5000))
+    props.createAnecdote(anecdote)
+    props.setNotification(`you created '${anecdote}'`, 5000)
   }
 
   return (
@@ -26,4 +24,11 @@ const AnecdoteForm = () => {
   )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = {
+  createAnecdote,
+  setNotification
+}
+
+const ConnectedAnecdoteForm = connect(null, mapDispatchToProps)(AnecdoteForm)
+
+export default ConnectedAnecdoteForm
