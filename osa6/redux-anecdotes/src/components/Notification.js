6919797ba0c77e4aff9a833cf90aec3a38516712
Timo Viipuri <timo.t@viipuri.eu>
2020-05-18 19:47:49 +0300

    6.19 anekdootit ja connect, step1 done
    Use old style "connect" method

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
-  return notification ? (
+  return props.notification ? (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   ) : ""
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
