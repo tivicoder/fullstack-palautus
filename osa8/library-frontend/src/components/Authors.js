41167fc5600c2f9363482f1862ece59a0cdae01a
Timo Viipuri <timo.t@viipuri.eu>
2020-09-09 17:22:27 +0300

    8.8: Kirjailijoiden näkymä done

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
 
 const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (result.loading) {
+    return(
+      <div> loading </div>
+    )
+  }
+
+  const authors = result.data.allAuthors
+  console.log('authors:', authors)
 
   return (
     <div>
