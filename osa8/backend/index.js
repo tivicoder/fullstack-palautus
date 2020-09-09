5bac2f5bbabf48c796c27dac72f9d79904c00f89
Timo Viipuri <timo.t@viipuri.eu>
2020-09-09 08:49:41 +0300

    8.5: genren kirjat done

--- osa8/backend/index.js
+++ osa8/backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => args.author ? books.filter(book => book.author === args.author) : books,
+    allBooks: (root, args) => {
+      let filtered = args.author ? books.filter(book => book.author === args.author) : books
+      return args.genre ? filtered.filter(book => book.genres.includes(args.genre)) : filtered
+    },
     allAuthors: () => authors
   },
 
